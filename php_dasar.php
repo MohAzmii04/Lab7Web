2bb0e82a03895cd4dc0205b9dcb0b84ce20c883b
MohAzmii04 <mohazmii04@gmail.com>
2023-11-20 19:00:42 +0700

    first commit

---
+++ php_dasar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP dasar</title>
+</head>
+<body>
+    <h1>Belajar PHP</h1>
+    <?php
+        echo "Hello World";
+    ?>
+<h3>Menggunakan Dua Variable</h3>
+        <?php
+    $nim = "312210109";
+    $nama = 'Mohammad Azmi Abdssyukur';
+    echo "NIM : " . $nim . "<br>";
+    echo "Nama : $nama";
+    ?>
+</body>
+</html>
